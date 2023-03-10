fbae714d244e17b7bade02c020162866472fb2e9
Alla <a.bondarenko@gmarkg.com>
2023-03-10 20:01:46 +0200

    add converter

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Converter from "./Converter";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Converter/>
+    </React.StrictMode>
+);
